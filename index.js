b79a6dfd33f066a4845ad9ae4f9e40bd78449915
RF-Rezon <rezwanfaysalrezon@gmail.com>
2023-05-02 04:56:56 +0600

    express routes initial set up done

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const jsonFile = require("./chefs.json")
-const port = 5000
+const jsonFile = require("./chefs.json");
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send("Server is running 🔥")
-})
+app.get("/", (req, res) => {
+  res.send("Server is running 🔥");
+});
 
-app.get('/all', (req, res) => {
-  res.send(jsonFile)
-})
+app.get("/all", (req, res) => {
+  res.send(jsonFile);
+});
+
+app.get("/all/:id", (req, res) => {
+  const id = req.params.id;
+  const recepies = jsonFile.find((each) => each.id == id);
+  res.send(recepies);
+});
+
+app.get("/all/:id/recepies", (req, res) => {
+  const id = req.params.id;
+  const obj = jsonFile.find((each) => each.id == id);
+  const allrecepies = obj.recipes.map(each=> each)
+  res.send(allrecepies);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
